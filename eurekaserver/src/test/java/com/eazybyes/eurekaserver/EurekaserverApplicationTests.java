fcf5dfe7844f36963de2b4852476b32c95e81dfc
Siyamcela Nxuseka <siyamcela.nxuseka@daimler.com>
2025-01-10 14:24:48 +0200

    Service Discovery Implementation

---
+++ eurekaserver/src/test/java/com/eazybyes/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybyes.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

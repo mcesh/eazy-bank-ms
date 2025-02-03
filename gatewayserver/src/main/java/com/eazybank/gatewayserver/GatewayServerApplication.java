9957ccb39c1bf7e94e2d13577546d5ea7b177181
Siyamcela Nxuseka <siyamcela.nxuseka@daimler.com>
2025-02-03 08:45:59 +0200

    Kubernetes Server Side Load Balancing

--- gatewayserver/src/main/java/com/eazybank/gatewayserver/GatewayServerApplication.java
+++ gatewayserver/src/main/java/com/eazybank/gatewayserver/GatewayServerApplication.java
@@ -2,6 +2,7 @@ package com.eazybank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -14,6 +15,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
@@ -27,19 +29,19 @@ public class GatewayServerApplication {
 						.path("/eazybank/accounts/**")
 						.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-						.uri("lb://ACCOUNTS"))
+						.uri("http://accounts:8080"))
 				.route(p -> p
 						.path("/eazybank/loans/**")
 						.filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET).setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000),2,true)))
-						.uri("lb://LOANS"))
+						.uri("http://loans:8085"))
 				.route(p -> p
 						.path("/eazybank/cards/**")
 						.filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(userKeyResolver())))
-						.uri("lb://CARDS"))
+						.uri("http://cards:8086"))
 				.build();
 	}
 
